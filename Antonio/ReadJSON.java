ca006c520366bc4dac02c22a0ce01aaf591b5727
MisterGas02 <118464750+MisterGas02@users.noreply.github.com>
2023-05-11 17:52:44 +0200

    Add files via upload

---
+++ Antonio/ReadJSON.java
@@ -0,0 +1,21 @@
+import java.io.FileReader;
+import java.io.IOException;
+/*import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;*/
+
+public class ReadJSON {
+  public static void main(String[] args) {
+    /*JSONParser parser = new JSONParser();
+    try {
+      JSONArray a = (JSONArray)parser.parse(new FileReader("the_guardian/..."));
+      JSONObject response = a.getJsonObject(0);
+      JSONArray fields = response.getJsonArray("results");
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }*/
+  }
+}
