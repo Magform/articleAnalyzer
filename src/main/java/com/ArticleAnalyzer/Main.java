3474283d2fe47d172c981cb9953fba4842694f06
Magform <nicolas25.02.2003.nf@gmail.com>
2023-05-22 12:50:34 +0200

    Comment on main

--- src/main/java/com/ArticleAnalyzer/Main.java
+++ src/main/java/com/ArticleAnalyzer/Main.java
@@ -8,28 +8,37 @@ import com.ArticleAnalyzer.Types.Library;
 
 public class Main {
   public static void main(String[] args) {
-
+    // Initialize variables
     Outputter outputter = null;
     ArticleLoader articleLoader = null;
     Elaborator elaborator = null;
 
-    try{
+    try {
+      // Parse command-line arguments
       Argparser argparser = new Argparser(args);
+
+      // Retrieve necessary components from the argparser
       outputter = argparser.getOutputter();
       articleLoader = argparser.getArticleLoader();
+      
+      // Load the articles into a Library
       Library toAnalyze = articleLoader.getLoadedLibrary();
+      
+      // Create an Elaborator instance with the loaded library
       elaborator = new Elaborator(toAnalyze);
-      if(argparser.getToExclude() == null){
+
+      // Check if any exclusion words were specified
+      if (argparser.getToExclude() == null) {
+        // If no exclusion words, print the word frequencies for the specified words
         outputter.print(elaborator.getWords(argparser.getToShow()));
-      }else{
+      } else {
+        // If exclusion words are provided, print the word frequencies for the specified words excluding the exclusion words
         outputter.print(elaborator.getWords(argparser.getToExclude(), argparser.getToShow()));
       }
-    }catch(Exception e){
-      System.err.println("Error: "+e);
-      System.exit(1);
+    } catch (Exception e) {
+      // Catch any exceptions that occur during the execution and print it
+      System.err.println("Error: " + e);
+      System.exit(1); // Terminate the program with an error status
     }
-
-
-
   }
 }
