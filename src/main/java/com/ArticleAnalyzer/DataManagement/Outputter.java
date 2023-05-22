9a5be0a7d6f35724e2165472b724d0385c8c779d
Magform <nicolas25.02.2003.nf@gmail.com>
2023-05-22 12:27:21 +0200

    OutputterTest added

--- src/main/java/com/ArticleAnalyzer/DataManagement/Outputter.java
+++ src/main/java/com/ArticleAnalyzer/DataManagement/Outputter.java
@@ -1,5 +1,31 @@
 package com.ArticleAnalyzer.DataManagement;
 
+/**
+ *
+ * The Outputter class provides functionality for printing Article objects, Library objects, and LinkedHashMap objects.
+ * It offers options to print the output to the console or to a file. The class supports various print methods for different types of data.
+ * 
+ * Public Constructors:
+ * - Outputter(): Constructs an Outputter object with default settings.
+ * - Outputter(boolean toConsole, boolean toFile): Constructs an Outputter object with specified console and file output options.
+ * - Outputter(boolean toConsole, boolean toFile, String file): Constructs an Outputter object with specified console and file output options, and sets the file path for printing to a file.
+ * 
+ * Public Methods:
+ * - getFile(): Returns the file path for outputting to a file.
+ * - getToConsole(): Returns a boolean indicating if outputting to the console is enabled.
+ * - setFile(String toSet): Sets the file path for outputting to a file.
+ * - getToFile(): Returns a boolean indicating if outputting to a file is enabled.
+ * - print(String toPrint): Prints a string to the console or file, based on the output options.
+ * - print(Article toPrint): Prints the details of an Article object to the console or file, based on the output options.
+ * - print(Library toPrint): Prints the details of all Article objects in a Library object to the console or file, based on the output options.
+ * - print(LinkedHashMap<String, Integer> toPrint): Prints the contents of a LinkedHashMap object to the console or file, based on the output options.
+ * 
+ * Private Methods:
+ * printToConsole(String toPrint): Prints a string to the console.
+ * printToFile(String toPrint): Prints a string to a file specified by the file path.
+ * check(): Checks the validity of the file path for outputting to a file.
+*/
+
 import java.io.FileWriter;
 import java.io.File;
 import java.util.LinkedHashMap;
@@ -13,42 +39,88 @@ public class Outputter {
     private boolean toFile;
     private String file;
 
+    /**
+     * Constructs an Outputter object with default settings.
+     * By default, printing to console and file is disabled.
+     */
     public Outputter() {
         this.toConsole = false;
         this.toFile = false;
         this.file = "";
     }
 
+    /**
+     * Constructs an Outputter object with the specified settings.
+     *
+     * @param toConsole flag indicating whether to print to console
+     * @param toFile    flag indicating whether to print to a file
+     * @throws IOException if an I/O error occurs
+     */
     public Outputter(boolean toConsole, boolean toFile) throws IOException {
         this.toConsole = toConsole;
         this.toFile = toFile;
         this.file = "";
     }
 
+    /**
+     * Constructs an Outputter object with the specified settings and file path.
+     *
+     * @param toConsole flag indicating whether to print to console
+     * @param toFile    flag indicating whether to print to a file
+     * @param file      the path of the file to print to
+     * @throws IOException if an I/O error occurs
+     */
     public Outputter(boolean toConsole, boolean toFile, String file) throws IOException {
         this(toConsole, toFile);
         this.file = file;
         check();
     }
 
+    /**
+     * Returns the file path for printing.
+     *
+     * @return the file path
+     */
     public String getFile(){
         return this.file;
     }
 
+    /**
+     * Returns whether printing to console is enabled.
+     *
+     * @return true if printing to console is enabled, false otherwise
+     */
     public boolean getToConsole(){
         return this.toConsole;       
     }
 
+    /**
+     * Sets the file path for printing.
+     *
+     * @param toSet the file path to set
+     * @throws IOException if an I/O error occurs
+     */
     public void setFile(String toSet) throws IOException{
         this.file = toSet;   
         check();    
     }
 
+    /**
+     * Returns whether printing to a file is enabled.
+     *
+     * @return true if printing to a file is enabled, false otherwise
+     */
     public boolean getToFile(){
         return this.toFile;
     }
 
-    //Funzioni di print di una stringa (le funzioni sono private per scelta implementativa)
+    
+    /**
+     * Prints a string to the console or file, based on the configured settings.
+     *
+     * @param toPrint the string to print
+     * @throws IOException if an I/O error occurs
+     */
     public void print(String toPrint) throws IOException{
         if(this.toConsole){
             this.printToConsole(toPrint);
@@ -58,11 +130,21 @@ public class Outputter {
         }
     }
 
+    /**
+     * Prints the given string to the console.
+     *
+     * @param toPrint the string to be printed
+     */
     private void printToConsole(String toPrint){
         System.out.println(toPrint);
     }
 
-    
+    /**
+     * Prints the given string to a file.
+     *
+     * @param toPrint the string to be printed
+     * @throws IOException if an error occurs while writing to the file
+     */
     private void printToFile(String toPrint) throws IOException{
         try {
             File file = new File(this.file);
@@ -75,6 +157,12 @@ public class Outputter {
         }
     }
 
+    /**
+     * Prints the properties of an Article object.
+     *
+     * @param toPrint the Article object to be printed
+     * @throws IOException if an error occurs while printing
+     */
     public void print(Article toPrint) throws IOException{
         print("{\n");
         if(!toPrint.getId().equals("")){
@@ -84,49 +172,49 @@ public class Outputter {
             print("\"type\" : \"" + toPrint.getType() + "\",\n");
         }
         if (!toPrint.getSectionID().equals("")) {
-            System.out.println("\"sectionID\" : \"" + toPrint.getSectionID() + "\",\n");
+            print("\"sectionID\" : \"" + toPrint.getSectionID() + "\",\n");
         }
         if (!toPrint.getWebUrl().equals("")) {
-            System.out.println("\"webUrl\" : \"" + toPrint.getWebUrl() + "\",\n");
+            print("\"webUrl\" : \"" + toPrint.getWebUrl() + "\",\n");
         }
         if (!toPrint.getApiUrl().equals("")) {
-            System.out.println("\"apiUrl\" : \"" + toPrint.getApiUrl() + "\",\n");
+            print("\"apiUrl\" : \"" + toPrint.getApiUrl() + "\",\n");
         }
         if (!toPrint.getTrailText().equals("")) {
-            System.out.println("\"trailText\" : \"" + toPrint.getTrailText() + "\",\n");
+            print("\"trailText\" : \"" + toPrint.getTrailText() + "\",\n");
         }
         if (!toPrint.getHeadline().equals("")) {
-            System.out.println("\"headline\" : \"" + toPrint.getHeadline() + "\",\n");
+            print("\"headline\" : \"" + toPrint.getHeadline() + "\",\n");
         }
         if (!toPrint.getByline().equals("")) {
-            System.out.println("\"byline\" : \"" + toPrint.getByline() + "\",\n");
+            print("\"byline\" : \"" + toPrint.getByline() + "\",\n");
         }
         if (!toPrint.getPublication().equals("")) {
-            System.out.println("\"publication\" : \"" + toPrint.getPublication() + "\",\n");
+            print("\"publication\" : \"" + toPrint.getPublication() + "\",\n");
         }
         if (!toPrint.getInternalPageCode().equals("")) {
-            System.out.println("\"internalPageCode\" : \"" + toPrint.getInternalPageCode() + "\",\n");
+            print("\"internalPageCode\" : \"" + toPrint.getInternalPageCode() + "\",\n");
         }
         if (!toPrint.getProductionOffice().equals("")) {
-            System.out.println("\"productionOffice\" : \"" + toPrint.getProductionOffice() + "\",\n");
+            print("\"productionOffice\" : \"" + toPrint.getProductionOffice() + "\",\n");
         }
         if (!toPrint.getBody().equals("")) {
-            System.out.println("\"body\" : \"" + toPrint.getBody() + "\",\n");
+            print("\"body\" : \"" + toPrint.getBody() + "\",\n");
         }
         if (!toPrint.getStandfirst().equals("")) {
-            System.out.println("\"standfirst\" : \"" + toPrint.getStandfirst() + "\",\n");
+            print("\"standfirst\" : \"" + toPrint.getStandfirst() + "\",\n");
         }
         if (!toPrint.getShortUrl().equals("")) {
-            System.out.println("\"shortUrl\" : \"" + toPrint.getShortUrl() + "\",\n");
+            print("\"shortUrl\" : \"" + toPrint.getShortUrl() + "\",\n");
         }
         if (!toPrint.getThumbnail().equals("")) {
-            System.out.println("\"thumbnail\" : \"" + toPrint.getThumbnail() + "\",\n");
+            print("\"thumbnail\" : \"" + toPrint.getThumbnail() + "\",\n");
         }
         if (!toPrint.getTitle().equals("")) {
-            System.out.println("\"title\" : \"" + toPrint.getTitle() + "\",\n");
+            print("\"title\" : \"" + toPrint.getTitle() + "\",\n");
         }
         if (!toPrint.getFullText().equals("")) {
-            System.out.println("\"fulltext\" : \"" + toPrint.getFullText() + "\",\n");
+            print("\"fulltext\" : \"" + toPrint.getFullText() + "\",\n");
         }
         if(!toPrint.getSourceSet().equals("")) {
             print("\"source_set\" : \"" + toPrint.getSourceSet() + "\",\n");
@@ -147,19 +235,19 @@ public class Outputter {
             print("\"lang\" : \"" + toPrint.getLang() + "\",\n");
         }  
         if (toPrint.getScore() != -1.0f) {
-            System.out.println("\"starRating\" : " + toPrint.getScore() + ",\n");
+            print("\"starRating\" : " + toPrint.getScore() + ",\n");
         }
         if (toPrint.getStarRating() != -1) {
-            System.out.println("\"starRating\" : " + toPrint.getStarRating() + ",\n");
+            print("\"starRating\" : " + toPrint.getStarRating() + ",\n");
         }
         if (toPrint.getNewspaperPageNumber() != -1) {
-            System.out.println("\"newspaperPageNumber\" : " + toPrint.getNewspaperPageNumber() + ",\n");
+            print("\"newspaperPageNumber\" : " + toPrint.getNewspaperPageNumber() + ",\n");
         }
         if (toPrint.getCharCount() != -1) {
-            System.out.println("\"charCount\" : " + toPrint.getCharCount() + ",\n");
+            print("\"charCount\" : " + toPrint.getCharCount() + ",\n");
         }
         if (toPrint.getWordcount() != -1) {
-            System.out.println("\"wordCount\" : " + toPrint.getWordcount() + ",\n");
+            print("\"wordCount\" : " + toPrint.getWordcount() + ",\n");
         }
         if (toPrint.getWebPublicationDate() != null) {
             print("\"webPublicationDate\": \"" + toPrint.getWebPublicationDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")) + "\"\n");
@@ -218,18 +306,35 @@ public class Outputter {
         print("}\n");
     }
 
+    /**
+     * Prints the articles in a Library object.
+     *
+     * @param toPrint the Library object containing articles to be printed
+     * @throws IOException if an error occurs while printing
+     */
     public void print(Library toPrint) throws IOException{
-        for(int i = 1; i < toPrint.getTotalArticleNumber(); i++){
+        for(int i = 1; i <= toPrint.getTotalArticleNumber(); i++){
             print(toPrint.getArticle(i));
         }
     }
 
+    /**
+     * Prints the key-value pairs in a LinkedHashMap.
+     *
+     * @param toPrint the LinkedHashMap to be printed
+     * @throws IOException if an error occurs while printing
+     */
     public void print(LinkedHashMap<String, Integer> toPrint) throws IOException{
         for (String key : toPrint.keySet()) {
             print(key + " " + toPrint.get(key)+"\n");
         }
     }
 
+    /**
+     * Checks if the necessary conditions are met before printing to a file.
+     *
+     * @throws IOException if the file is not set or cannot be created
+     */
     private void check() throws IOException{
         if(toFile){
             if(this.file==""){
---
+++ src/test/java/com/ArticleAnalyzer/DataManagement/OutputterTest.java
@@ -0,0 +1,280 @@
+package com.ArticleAnalyzer.DataManagement;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.LinkedHashMap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.ArticleAnalyzer.Types.Article;
+import com.ArticleAnalyzer.Types.Library;
+
+@RunWith(JUnit4.class)
+public class OutputterTest {
+
+    //Test for default constructor
+    @Test
+    public void defaultConstructor(){
+        Outputter test = new Outputter();
+        assertEquals("", test.getFile());
+        assertEquals(false, test.getToConsole());
+        assertEquals(false, test.getToFile());
+    }
+
+    @Test
+    public void constructorNoFile() throws IOException{
+        Outputter test = new Outputter(false, false);
+        assertEquals("", test.getFile());
+        assertEquals(false, test.getToConsole());
+        assertEquals(false, test.getToFile());
+        test = new Outputter(true, true);
+        assertEquals("", test.getFile());
+        assertEquals(true, test.getToConsole());
+        assertEquals(true, test.getToFile());
+    }
+
+    @Test
+    public void constructorWithFile() throws IOException{
+        Outputter test = new Outputter(false, false, "test.txt");
+        assertEquals("test.txt", test.getFile());
+        assertEquals(false, test.getToConsole());
+        assertEquals(false, test.getToFile());
+        test = new Outputter(true, true, "test.txt");
+        assertEquals(true, test.getToConsole());
+        assertEquals(true, test.getToFile());
+        assertEquals("test.txt", test.getFile());
+    }
+
+    @Test
+    public void testToFile() throws IOException{
+        Outputter test = new Outputter(false, false);
+        assertEquals("", test.getFile());
+        test.setFile("test.txt");
+        assertEquals("test.txt", test.getFile());
+    }
+
+    //Test printing to console
+    @Test
+    public void testPrintStringToConsole() throws IOException{
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        Outputter test = new Outputter(true, false);
+        test.print("test test test");
+
+        System.out.flush();
+        System.setOut(originalOut);
+        String printedOutput = outputStream.toString().trim();
+        assertEquals("test test test", printedOutput);
+    }
+
+    @Test
+    public void testPrintArticleToConsole() throws IOException{
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        Article testArticle = new Article();
+        testArticle.setId("test");
+        testArticle.setBody("test");
+
+        Outputter test = new Outputter(true, false);
+        test.print(testArticle);
+
+        System.out.flush();
+        System.setOut(originalOut);
+        String printedOutput = outputStream.toString().trim();
+
+        String expected = "{\"id\":\"test\",\"body\":\"test\",}";
+        expected = expected.replaceAll("\\s+", "");
+        printedOutput = printedOutput.replaceAll("\\s+", "");
+
+        assertEquals(expected, printedOutput);
+    }
+
+    @Test
+    public void testPrintLibraryToConsole() throws IOException{
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        Article testArticle1 = new Article();
+        testArticle1.setId("test1");
+        Article testArticle2 = new Article();
+        testArticle2.setId("test2");
+
+        Library testLibrary = new Library(testArticle1);
+        testLibrary.addArticle(testArticle2);
+
+        Outputter test = new Outputter(true, false);
+        test.print(testLibrary);
+
+        System.out.flush();
+        System.setOut(originalOut);
+        String printedOutput = outputStream.toString().trim();
+
+        String expected = "{\"id\":\"test1\",}{\"id\":\"test2\",}";
+        expected = expected.replaceAll("\\s+", "");
+        printedOutput = printedOutput.replaceAll("\\s+", "");
+
+        assertEquals(expected, printedOutput);
+    }
+
+    @Test
+    public void testPrintHashMapToConsole() throws IOException{
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        LinkedHashMap<String, Integer> testHashMap = new LinkedHashMap<>();       
+
+        testHashMap.put("test1", 1);
+        testHashMap.put("test2", 2);
+
+        Outputter test = new Outputter(true, false);
+        test.print(testHashMap);
+
+        System.out.flush();
+        System.setOut(originalOut);
+        String printedOutput = outputStream.toString().trim();
+
+        String expected = "test1 1 \n test2 2";
+        expected = expected.replaceAll("\\s+", "");
+        printedOutput = printedOutput.replaceAll("\\s+", "");
+
+        assertEquals(expected, printedOutput);
+    }
+
+    //Test printing to file
+    @Test
+    public void testPrintStringToFile() throws IOException{
+        
+
+        Outputter test = new Outputter(false, true, "test.txt");
+        test.print("test test test");
+
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    content.append(System.lineSeparator()); // Append a new line for empty lines
+                } else {
+                    content.append(line);
+                }
+            }
+        }
+
+        // Perform assertions on the file content
+        String expected = "test test test"; // Replace with the expected output
+        assertEquals(expected, content.toString());
+
+    }
+
+    @Test
+    public void testPrintArticleToFile() throws IOException{
+
+        Article testArticle = new Article();
+        testArticle.setId("test");
+        testArticle.setBody("test");
+
+        Outputter test = new Outputter(false, true, "test.txt");
+        test.print(testArticle);
+
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    content.append(System.lineSeparator()); // Append a new line for empty lines
+                } else {
+                    content.append(line);
+                }
+            }
+        }
+
+        String printedOutput = content.toString();
+        String expected = "{\"id\":\"test\",\"body\":\"test\",}";
+        expected = expected.replaceAll("\\s+", "");
+        printedOutput = printedOutput.replaceAll("\\s+", "");
+
+        assertEquals(expected, printedOutput);
+    }
+
+    @Test
+    public void testPrintLibraryToFile() throws IOException{
+
+        Article testArticle1 = new Article();
+        testArticle1.setId("test1");
+        Article testArticle2 = new Article();
+        testArticle2.setId("test2");
+
+        Library testLibrary = new Library(testArticle1);
+        testLibrary.addArticle(testArticle2);
+
+        Outputter test = new Outputter(false, true, "test.txt");
+        test.print(testLibrary);
+        
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    content.append(System.lineSeparator()); // Append a new line for empty lines
+                } else {
+                    content.append(line);
+                }
+            }
+        }
+
+        String printedOutput = content.toString();
+        String expected = "{\"id\":\"test1\",}{\"id\":\"test2\",}";
+        expected = expected.replaceAll("\\s+", "");
+        printedOutput = printedOutput.replaceAll("\\s+", "");
+
+        assertEquals(expected, printedOutput);
+    }
+
+    @Test
+    public void testPrintHashMapToFile() throws IOException{
+
+        LinkedHashMap<String, Integer> testHashMap = new LinkedHashMap<>();       
+
+        testHashMap.put("test1", 1);
+        testHashMap.put("test2", 2);
+
+        Outputter test = new Outputter(false, true, "test.txt");
+        test.print(testHashMap);
+
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    content.append(System.lineSeparator()); // Append a new line for empty lines
+                } else {
+                    content.append(line);
+                }
+            }
+        }
+
+        String printedOutput = content.toString();
+        String expected = "test1 1 \n test2 2";
+        expected = expected.replaceAll("\\s+", "");
+        printedOutput = printedOutput.replaceAll("\\s+", "");
+
+        assertEquals(expected, printedOutput);
+    }
+}
