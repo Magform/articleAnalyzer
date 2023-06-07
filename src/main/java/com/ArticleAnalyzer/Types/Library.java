ca950534c4f41817650affc29cfc6f1020789042
MisterGas02 <118464750+MisterGas02@users.noreply.github.com>
2023-06-07 17:45:55 +0200

    Update Library.java

--- src/main/java/com/ArticleAnalyzer/Types/Library.java
+++ src/main/java/com/ArticleAnalyzer/Types/Library.java
@@ -1,28 +1,28 @@
 package com.ArticleAnalyzer.Types;
 
 /**
- * The Library class represents a collection of Article objects and provides various functions to interact with the collection.
- */
+ * The Library class represents a collection of Article objects and provides various functions in order to interact with the collection.
+*/
 public class Library {
     private int totalArticlesNumber;
     private int scannedArticles;
     private Article[] collection;
-    private static final int INITIAL_CAPACITY = 10; // choose the initial size of the collection with a fixed size of 10
+    private static final int INITIAL_CAPACITY = 10; //initial size of the collection with a fixed size of 10
 
     /**
      * Creates a Library object with an initial collection of articles and initializes other attributes to their default values.
-     */
+    */
     public Library() {
         totalArticlesNumber = 0;
         scannedArticles = 0;
         collection = new Article[INITIAL_CAPACITY];
     }
-   /**
+    
+    /**
      * Creates a Library object with an initial collection containing the given Article object.
-     *
+     * @see addArticle(Article)
      * @param article the Article object to add to the collection
-     * @see #addArticle(Article)
-     */
+    */
     public Library(Article article) {
         this();
         addArticle(article);
@@ -30,12 +30,10 @@ public class Library {
 
     /**
      * Adds the given Article object to the collection.
-     * If the collection is full, the size of the collection is doubled before adding the Article object.
-     *
+     * If the collection is full, the size of the collection is doubled before adding the Article object
      * @param article the Article object to add to the collection
-     */
-    public void addArticle(Article article){
-        //Check if collection[] is full, if yes it double it's lenght
+    */
+    public void addArticle(Article article) {
         if (totalArticlesNumber == collection.length) {
             Article[] newCollection = new Article[collection.length * 2];
             System.arraycopy(collection, 0, newCollection, 0, collection.length);
@@ -47,36 +45,33 @@ public class Library {
 
     /**
      * Returns the total number of available Article objects in the collection.
-     *
      * @return the total number of available Article objects
-     */
-    public int getTotalArticleNumber(){
-        return this.totalArticlesNumber;
+    */
+    public int getTotalArticlesNumber() {
+        return totalArticlesNumber;
     }
 
     /**
      * Returns the Article object at the specified index.
-     *
      * @param articleNumber the index of the Article object to retrieve
      * @return the Article object at the specified index
      * @throws IllegalArgumentException if the article number is invalid
-     */
-    public Article getArticle(int articleNumber) throws IllegalArgumentException{
-        if(articleNumber <= 0 || articleNumber > totalArticlesNumber){
-            throw new IllegalArgumentException("Invalid articleNumber");
+    */
+    public Article getArticle(int articleNumber) throws IllegalArgumentException {
+        if (articleNumber < 0 || articleNumber >= totalArticlesNumber) {
+            throw new IllegalArgumentException("Invalid article number");
         }
-        return this.collection[articleNumber - 1];
+        return collection[articleNumber];
     }
 
     /**
-     * Retrieves the next Article object in the collection based on an internal index.
+     * Retrieves the next Article object in the collection basing on an internal index.
      * Returns null if there are no more articles to retrieve.
-     *
      * @return the next Article object in the collection, or null if there are no more articles
-     */
-    public Article getNextArticle(){
+    */
+    public Article getNextArticle() {
         if (scannedArticles == totalArticlesNumber) {
-            return null; // no more articles to scan
+            return null; //no more articles to scan
         }
         scannedArticles++;
         return collection[scannedArticles - 1];
@@ -84,11 +79,9 @@ public class Library {
 
     /**
      * Resets the internal index for the getNextArticle() function, allowing it to start from the first article again.
-     *
      * @see #getNextArticle()
-     */
-    public void resetScannedArticle(){
+    */
+    public void resetScannedArticle() {
         scannedArticles = 0;
     }
-
 }
