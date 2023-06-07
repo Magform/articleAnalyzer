122b145edd4c0c291c617b1a08951a72df1529b0
MisterGas02 <118464750+MisterGas02@users.noreply.github.com>
2023-06-07 18:51:39 +0200

    Update Outputter.java

--- src/main/java/com/ArticleAnalyzer/DataManagement/Outputter.java
+++ src/main/java/com/ArticleAnalyzer/DataManagement/Outputter.java
@@ -10,213 +10,179 @@ import com.ArticleAnalyzer.Types.Library;
 
 /**
  * The Outputter class provides functionality for printing various objects such as strings, Article objects, Library objects, and LinkedHashMap objects.
- *  It offers options to print the output to the console or to a file.
- */
+ * It offers options to print the output to the console or to a file.
+*/
 public class Outputter {
     private boolean toConsole;
     private boolean toFile;
     private String file;
 
     /**
-     * Initializes all attributes of the Outputter class to their default values.
-     */
+     * Initializes all attributes to their default values.
+    */
     public Outputter() {
-        this.toConsole = false;
-        this.toFile = false;
-        this.file = "";
+        toConsole = false;
+        toFile = false;
+        file = "";
+    }
+    
+    /**
+     * Initializes the options to print output to the console or to a file.
+     * @param tc the option to print the output to the console
+     * @param tf the option to print the output to a file
+    */
+    public Outputter(boolean tc, boolean tf) {
+        toConsole = tc;
+        toFile = tf;
+        file = "";
     }
 
-  /**
-    * Initializes the options to print output to the console or to a file.
-    *
-    * @param tc the option to print the output to the console
-    * @param tf the option to print the output to a file
+    /**
+     * Initializes the file path with the given value
+     * @see Outputter(boolean tc, boolean tf)
+     * @param tc the option to print the output to the console
+     * @param tf the option to print the output to a file
+     * @param fileName the file path
+     * @throws IOException if the option to print to a file is active and the file path is not specified
     */
-  public Outputter(boolean tc, boolean tf) {
-    toConsole = tc;
-    toFile = tf;
-    file = "";
-  }
-
-  /**
-    * Initializes the file path with the given value
-    * 
-    * @see Outputter(boolean tc, boolean tf)
-    * @param tc the option to print the output to the console
-    * @param tf the option to print the output to a file
-    * @param fn the file path
-    * @throws IOException if the option to print to a file is active and the file path is not specified
-    */
-  public Outputter(boolean tc, boolean tf, String fileName) throws IOException {
-    this(tc, tf);
-    setFile(fileName);
- }
+    public Outputter(boolean tc, boolean tf, String fileName) throws IOException {
+        this(tc, tf);
+        setFile(fileName);
+    }
 
     /**
      * Returns the file path.
-     *
      * @return the file path
-     */
-    public String getFile(){
-        return this.file;
+    */
+    public String getFile() {
+        return file;
     }
 
     /**
      * Returns whether printing to console is enabled.
-     *
      * @return true if printing to console is enabled, false otherwise.
      */
-    public boolean getToConsole(){
-        return this.toConsole;       
+    public boolean getToConsole() {
+        return toConsole;       
     }
 
     /**
      * Returns whether printing to a file is enabled.
-     *
      * @return true if printing to a file is enabled, false otherwise.
      */
-    public boolean getToFile(){
-        return this.toFile;
+    public boolean getToFile() {
+        return toFile;
     }
 
     /**
      * Sets the file path for printing.
-     *
-     * @param toSet the file path to set
+     * @param fileName the file path to set
      * @throws IOException if an I/O error occurs
-     */
-    public void setFile(String toSet) throws IOException{
-        this.file = toSet;   
+    */
+    public void setFile(String fileName) throws IOException {
+        file = fileName;   
         check();    
     }
     
     /**
       * Prints the given string to the console or to a file, based on the values assumed by the class attributes
-      *
-      * @see printToConsole(String s)
-      * @see printToFile(String s)
+      * @see printToConsole(String)
+      * @see printToFile(String)
       * @param toPrint the string to print to the console or to a file, based on to the values assumed by the class attributes
       * @throws IOException if there are errors while writing the file
-      */
-    public void print(String toPrint) throws IOException{
-        if(this.toConsole){
-            this.printToConsole(toPrint);
+    */
+    public void print(String toPrint) throws IOException {
+        if (toConsole) {
+            printToConsole(toPrint);
         }
-        if(this.toFile){
-            this.printToFile(toPrint);
+        if (toFile) {
+            printToFile(toPrint);
         }
     }
 
     /**
      * Prints the given string to console.
-     *
      * @param toPrint the string to be printed
      */
-    private void printToConsole(String toPrint){
+    private void printToConsole(String toPrint) {
         System.out.print(toPrint);
     }
 
     /**
      * Prints the given string to a file.
-     *
      * @param toPrint the string to be printed
      * @throws IOException if an error occurs while writing to the file
      */
-    private void printToFile(String toPrint) throws IOException{
+    private void printToFile(String toPrint) throws IOException {
         try {
-            File file = new File(this.file);
-            file.createNewFile();
-            FileWriter fileWriter = new FileWriter(this.file, true);
+            File f = new File(file);
+            f.createNewFile();
+            FileWriter fileWriter = new FileWriter(file, true);
             fileWriter.write(toPrint);
             fileWriter.close();
-        } catch (IOException e) {
-            throw new IOException("Error writing to file: " + this.file, e);
+        }
+        catch (IOException e) {
+            throw new IOException("Error writing to file: " + file);
         }
     }
 
     /**
      * Prints the properties of an Article object.
-     * 
-     * @see #print(Article)
+     * @see print(Article)
      * @param toPrint the Article object to be printed
      * @throws IOException if an error occurs while printing
-     */
-    public void print(Article toPrint) throws IOException{
+    */
+    public void print(Article toPrint) throws IOException {
         print("{");
-        if(!toPrint.getId().equals("")){
-            print("\"id\" : \"" + toPrint.getId() + "\",");
+        if (!toPrint.getIdentifier().equals("")) {
+            print("\"id\" : \"" + toPrint.getIdentifier() + "\",");
         }
-        if(!toPrint.getType().equals("")){
-            print("\"type\" : \"" + toPrint.getType() + "\",");
+        if (!toPrint.getSection().equals("")) {
+            print("\"section\" : \"" + toPrint.getSection() + "\",");
         }
-        if (!toPrint.getSectionID().equals("")) {
-            print("\"sectionID\" : \"" + toPrint.getSectionID() + "\",");
+        if (!toPrint.getSource().equals("")) {
+            print("\"source\" : \"" + toPrint.getSource() + "\",");
         }
-        if (!toPrint.getWebUrl().equals("")) {
-            print("\"webUrl\" : \"" + toPrint.getWebUrl() + "\",");
+        if (!toPrint.getPublicationDate().equals("")) {
+            print("\"publicationDate\" : \"" + toPrint.getPublicationDate() + "\",");
         }
-        if (!toPrint.getApiUrl().equals("")) {
-            print("\"apiUrl\" : \"" + toPrint.getApiUrl() + "\",");
+        if (!toPrint.getLanguage().equals("")) {
+            print("\"language\" : \"" + toPrint.getLanguage() + "\",");
         }
-        if (!toPrint.getTrailText().equals("")) {
-            print("\"trailText\" : \"" + toPrint.getTrailText() + "\",");
-        }
-        if (!toPrint.getHeadline().equals("")) {
-            print("\"headline\" : \"" + toPrint.getHeadline() + "\",");
-        }
-        if (!toPrint.getProductionOffice().equals("")) {
-            print("\"productionOffice\" : \"" + toPrint.getProductionOffice() + "\",");
-        }
-        if (!toPrint.getBody().equals("")) {
-            print("\"body\" : \"" + toPrint.getBody() + "\",");
-        }
-        if (!toPrint.getStandfirst().equals("")) {
-            print("\"standfirst\" : \"" + toPrint.getStandfirst() + "\",");
-        }
-        if (!toPrint.getShortUrl().equals("")) {
-            print("\"shortUrl\" : \"" + toPrint.getShortUrl() + "\",");
+        if (!toPrint.getUrl().equals("")) {
+            print("\"url\" : \"" + toPrint.getUrl() + "\",");
         }
         if (!toPrint.getTitle().equals("")) {
             print("\"title\" : \"" + toPrint.getTitle() + "\",");
         }
-        if(!toPrint.getMain().equals("")) {
-            print("\"main\" : \"" + toPrint.getMain() + "\",");
+        if (!toPrint.getSubtitle().equals("")) {
+            print("\"subtitle\" : \"" + toPrint.getSubtitle() + "\",");
         }
-        if(!toPrint.getBodyText().equals("")) {
-            print("\"body_text\" : \"" + toPrint.getBodyText() + "\",");
+        if (!toPrint.getBody().equals("")) {
+            print("\"body\" : \"" + toPrint.getBody() + "\",");
         }
-        if(!toPrint.getLang().equals("")) {
-            print("\"lang\" : \"" + toPrint.getLang() + "\",");
-        }  
-        if (toPrint.getNewspaperPageNumber() != -1) {
-            print("\"newspaperPageNumber\" : " + toPrint.getNewspaperPageNumber() + ",");
+        if (!toPrint.getNewspaperPage().equals("")) {
+            print("\"newspaperPage\" : \"" + toPrint.getNewspaperPage() + "\",");
         }
-        if (toPrint.getCharCount() != -1) {
-            print("\"charCount\" : " + toPrint.getCharCount() + ",");
-        }
-        if (toPrint.getWordcount() != -1) {
-            print("\"wordCount\" : " + toPrint.getWordcount() + ",");
-        }
-        if (toPrint.getWebPublicationDate() != null) {
-            print("\"webPublicationDate\": \"" + toPrint.getWebPublicationDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")) + "\"");
+        if (!toPrint.getWords().equals("")) {
+            print("\"words\" : \"" + toPrint.getWords() + "\"");
         }
         print("}");
     }
 
     /**
      * Prints a Library object.
-     *
-     * @see #print(Article)
+     * @see print(Article)
      * @param toPrint the Library object containing articles to be printed
      * @throws IOException if an error occurs while printing
      */
-    public void print(Library toPrint) throws IOException{
+    public void print(Library toPrint) throws IOException {
         print("{\n");
         print("\"article\":\n");
         print("[\n");
-        for(int i = 1; i <= toPrint.getTotalArticleNumber(); i++){
+        for (int i = 1; i <= toPrint.getTotalArticlesNumber(); i++) {
             print(toPrint.getArticle(i));
-            if(i!=toPrint.getTotalArticleNumber()){
+            if (i != toPrint.getTotalArticlesNumber()) {
                 print(",");
             }
         }
@@ -226,29 +192,27 @@ public class Outputter {
 
     /**
      * Prints the given LinkedHashMap object with key-value pairs.
-     *
      * @param toPrint the LinkedHashMap object with key-value pairs to print
      * @throws IOException if an error occurs while printing
      */
-    public void print(LinkedHashMap<String, Integer> toPrint) throws IOException{
+    public void print(LinkedHashMap<String, Integer> toPrint) throws IOException {
         for (String key : toPrint.keySet()) {
-            print(key + " " + toPrint.get(key)+"\n");
+            print(key + " " + toPrint.get(key) + "\n");
         }
     }
 
     /**
       * Checks and throws the IOException exception if the option to print to a file is active and the file path is not specified.
       * It also takes care of recreating the file if it already exists or simply creating it if it doesn't exist
-      *
       * @throws IOException if the option to print to a file is active and the file path is not specified
-      */
-    private void check() throws IOException{
-        if(toFile){
-            if(this.file==""){
-                throw new IOException("File were to print not setted");
+    */
+    private void check() throws IOException {
+        if (toFile) {
+            if (file.equals("")) {
+                throw new IOException("File where to print not setted");
             }
-            File file = new File(this.file);
-            if (file.exists()){
+            File f = new File(file);
+            if (file.exists()) {
                 file.delete();
             }
             file.createNewFile();
